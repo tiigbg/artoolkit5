37badd7ee692f3703e5f3e0d0f079bcd5cc7fc56
Jacob Michelsen <chromegadget@gmail.com>
2016-06-18 11:28:08 +0200

    Trying to get the iOS project to work, committing before running RNPM commands

---
+++ Augus.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Provider, connect } from 'react-redux';
+import { Modal, Actions, Scene, Router } from 'react-native-router-flux';
+
+import TabIcon from './components/TabIcon.js';
+import ExhibitionList from './containers/ExhibitionList';
+import StationList from './containers/StationList';
+import StationScreen from './components/StationScreen';
+import Map from './containers/Map';
+
+
+import { configureStore } from './store/Store.js';
+
+const RouterWithRedux = connect()(Router);
+
+const scenes = Actions.create(
+  <Scene key="modal" component={Modal} >
+    <Scene key="root" hideNavBar={true}>
+      <Scene key="tabbar" tabs={true} default="tab1" >
+        <Scene key="tab1" title="Stories" icon={TabIcon} icontype="leaf">
+          <Scene key="exhibitonList" component={ExhibitionList} title="Utställningar" initial={ true } />
+          <Scene key="stationList" component={StationList} title="Station List"  />
+          <Scene key="stationScreen" component={StationScreen} title="StationScreen" />
+        </Scene>
+        <Scene key="tab2" title="Nearby" icon={TabIcon} icontype="map">
+          <Scene key="map" component={Map} title="Nearby Stories" initial={ true } />
+        </Scene>
+      </Scene>
+    </Scene>
+  </Scene>
+);
+
+export default class Augus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {store: configureStore()};
+  }
+  render() {
+    return (
+        <Provider store={this.state.store}>
+        <RouterWithRedux scenes={scenes} />
+      </Provider>
+    );
+  }
+}
